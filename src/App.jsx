efe8bd27a215b8938f0beebca3fe55d037bba06a
Maryam <masumaakhtermaryam@gmail.com>
2026-09-12 19:31:43 +0600

    chore: scaffold React project with Vite and first static component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProductCard from "./components/ProductCard"
+
+function App() {
+    return (
+        <div>
+            <ProductCard />
+        </div>
+    )
+}
+
+
+export default App
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,11 @@
+export default function ProductCard () {
+      return (
+        <article className="card">
+            <img src="/assets/crossbody-bag.jpg" alt="Crossbody Sling Bag" />
+            <h3>Crossbody Sling Bag</h3>
+            <p>A timeless leather bag for everyday elegance.</p>
+            <p className="price">৳ 1450</p>
+            <button>Add to Cart</button>
+        </article>
+    )
+}
