8fcad8e727e972f90ee242021af328546828a2e5
Maryam <masumaakhtermaryam@gmail.com>
2026-09-13 13:04:47 +0600

    feat: Category filtering with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import { products } from "./data/product"
 import ProductCard from "./components/ProductCard"
+import { useState } from "react"
 
 function App() {
+    const [activeCategory, setActiveCategory] = useState("all")
+    const categories = ["all", "Bags", "Clothing", "Jewelry", "Shoes"]
+
+        const filteredProducts = activeCategory === "all"
+        ? products
+        : products.filter(product => product.category === activeCategory)
+
     return (
+        <>
+            <div className="filters">
+                {categories.map(category => (
+                    <button
+                        className ={activeCategory === category? "active": ""}
+                        onClick={() => setActiveCategory(category)}
+                    >
+                        {category}
+                    </button>
+                ))}
+            </div>
+
         <div id="product-grid">
-            {products.map(product => (
+            {filteredProducts.map(product => (
                 <ProductCard key={product.id} product={product} />
             ))}            
         </div>
+        </>    
     )
 }
 
